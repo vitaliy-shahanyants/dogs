06ee47ba944fababf71de75b010e41cc29e2a316
Vitaliy Shahanyants <vi_taliy@hotmail.com>
2019-09-25 09:24:27 -0400

    Adding Navigation Between List Fragment and Detail fragment and animation

---
+++ app/src/main/java/ca/vitos/dogs/view/ListFragment.kt
@@ -0,0 +1,34 @@
+package ca.vitos.dogs.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import ca.vitos.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            action.dogUid = 5
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}

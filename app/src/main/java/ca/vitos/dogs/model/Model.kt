59f68aced53704dc78e4562eacd6e11800a9145e
Vitaliy Shahanyants <vi_taliy@hotmail.com>
2019-09-30 11:20:35 -0400

    Adding SerializedName to the DoogBreed Model

--- app/src/main/java/ca/vitos/dogs/model/Model.kt
+++ app/src/main/java/ca/vitos/dogs/model/Model.kt
@@ -1,12 +1,20 @@
 package ca.vitos.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DoogBreed(
 
+    @SerializedName(value = "id")
     val breedID: String?,
+    @SerializedName(value = "name")
     val dogBreed: String?,
+    @SerializedName(value = "life_span")
     val lifeSpan: String?,
+    @SerializedName(value = "bred_for")
     val bredFor: String?,
+    @SerializedName(value= "temperament")
     val temperament: String?,
+    @SerializedName(value = "url")
     val imageUrl: String?
 
 
